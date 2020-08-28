79eb20e731ef1a433d72d9c7bb30dbfce7d3e70e
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-08-28 15:24:14 -0400

    decode finished

--- bases.py
+++ bases.py
@@ -7,7 +7,7 @@ import string
 # string.ascii_letters is ascii_lowercase + ascii_uppercase
 # string.printable is digits + ascii_letters + punctuation + whitespace
 
-
+#Binary to decimal
 def decode(digits, base):
     """Decode given digits in given base to number in base 10.
     digits: str -- string representation of number (in given base)
@@ -15,21 +15,23 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
-    # TODO: Decode digits from binary (base 2)
-    #Binary to decimal
-    digits = digits[::-1]
+
     sum = 0
+    #loop through the digits to find the match indx of input value
     for i in range(len(digits)):
-        result = int(digits[i]) * base ** i
-        sum += result
+        #check if digits contain letters
+        if digits[i].isalpha():
+            #if yes, use .index to find the index of the match letter
+            value = string.ascii_lowercase.index(digits[i].lower()) + 10 #added 10 because 0-9 before Hex string
+        else:
+            #if not, use int to convert number into decimal string.
+            value = int(digits[i])
+            #get sum by adding value * base to whatever power it is base on the length of the digits.
+        sum += value * (base ** (len(digits) -i - 1)) #-1 to reverse the digits since read from righ to left.
+        
     return sum
 
-    # TODO: Decode digits from hexadecimal (base 16)
-    # ...
-    # TODO: Decode digits from any base (2 up to 36)
-    # ...
-
-
+#decimal to binary or anyother bases
 def encode(number, base):
     """Encode given number in base 10 to digits in given base.
     number: int -- integer representation of number (in base 10)
@@ -39,12 +41,29 @@ def encode(number, base):
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
-    # TODO: Encode number in binary (base 2)
-    # ...
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
+
+    #loop to do the repeated divison, where to stop?
+    #get the remainders and divisors
+    #save theremainders
+    #return the final numbe result as a string
+    #somehow deal with hex digits
+
+    digits_and_letters = string.digits + string.ascii_letters
+    print(digits_and_letters)
+    new_number = number
+    final_digits = ""
+    while new_number > 0:
+        #print("divisor", new_number)
+        remainder = new_number % base
+        if base == 16:
+            remainder = digits_and_letters[remainder]
+        print("remainder", remainder)
+        new_number = new_number // base
+        final_digits += str(remainder)
+    #print(final_digits)
+    return final_digits[::-1]
+    
+        
 
 
 def convert(digits, base1, base2):
@@ -63,7 +82,7 @@ def convert(digits, base1, base2):
     # TODO: Convert digits from base 10 to base 16 (and vice versa)
     # ...
     # TODO: Convert digits from any base to any base (2 up to 36)
-    # ...
+    
 
 
 def main():
@@ -83,4 +102,9 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    print(encode('1',2)) #1
+    print(encode('5',10)) #5
+    print(encode('10',16)) #a
+    print(encode('9781876',25)) #101101
+
